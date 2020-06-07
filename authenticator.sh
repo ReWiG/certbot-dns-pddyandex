ad4861fb2cabd0c28497841f787e6a02ef8be457
ReWiG <ReWiG@users.noreply.github.com>
2020-06-07 16:33:13 +0300

    Update authenticator.sh

--- authenticator.sh
+++ authenticator.sh
@@ -5,7 +5,7 @@ _dir="$(dirname "$0")"
 source "$_dir/config.sh"
 
 # Strip only the top domain to get the zone id
-DOMAIN=$(expr match "$CERTBOT_DOMAIN" '.*\.\(.*\..*\)')
+# DOMAIN=$(expr match "$CERTBOT_DOMAIN" '.*\.\(.*\..*\)')
 	
 # Create TXT record
 CREATE_DOMAIN="_acme-challenge"
@@ -14,13 +14,13 @@ RECORD_ID=$(curl -s -X POST "https://pddimp.yandex.ru/api2/admin/dns/add" \
      -d "domain=$CERTBOT_DOMAIN&type=TXT&content=$CERTBOT_VALIDATION&ttl=60&subdomain=$CREATE_DOMAIN" \
 	 | python -c "import sys,json;print(json.load(sys.stdin)['record']['record_id'])")
 	
-if [ ! -d /tmp/CERTBOT_temp ];then
-        mkdir -m 0700 /tmp/CERTBOT_temp
+if [ ! -d /tmp/CERTBOT_count ];then
+        mkdir -m 0700 /tmp/CERTBOT_count
 fi
 
-COUNT_RECORD=1
-if [ -f /tmp/CERTBOT_$CERTBOT_DOMAIN/COUNT_RECORD ]; then
-        COUNT_RECORD=$(cat /tmp/CERTBOT_$CERTBOT_DOMAIN/COUNT_RECORD)
+COUNT_RECORD=0
+if [ -f /tmp/CERTBOT_count/COUNT_RECORD ]; then
+        COUNT_RECORD=$(cat /tmp/CERTBOT_count/COUNT_RECORD)
 fi
 
 # Save info for cleanup
@@ -29,12 +29,11 @@ if [ ! -d /tmp/CERTBOT_$CERTBOT_DOMAIN ];then
 fi
 
 echo $RECORD_ID > /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID_${COUNT_RECORD}
-echo (($COUNT_RECORD+1)) > /tmp/CERTBOT_temp/COUNT_RECORD
+let "COUNT_RECORD += 1"
+echo $COUNT_RECORD > /tmp/CERTBOT_count/COUNT_RECORD
 
 # Sleep to make sure the change has time to propagate over to DNS
 
 if [ "$COUNT_RECORD" -ne "1" ];then
 	sleep 600
-else
-	sleep 5
 fi
