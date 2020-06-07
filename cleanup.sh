25cbae53bec08d93e2a5a042b55655418bb0e117
ReWiG <ReWiG@users.noreply.github.com>
2020-06-07 17:07:11 +0300

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -4,9 +4,15 @@ _dir="$(dirname "$0")"
 
 source "$_dir/config.sh"
 
-if [ -f /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID ]; then
-        RECORD_ID=$(cat /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID)
-        rm -f /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID
+if [ -f /tmp/CERTBOT_count/COUNT_RECORD ]; then
+        COUNT_RECORD=$(cat /tmp/CERTBOT_count/COUNT_RECORD)
+fi
+
+let "COUNT_RECORD -= 1"
+
+if [ -f /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID_${COUNT_RECORD} ]; then
+        RECORD_ID=$(cat /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID_${COUNT_RECORD})
+        rm -f /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID_${COUNT_RECORD}
 fi
 
 # Remove the challenge TXT record from the zone
@@ -18,4 +24,8 @@ if [ -n "${RECORD_ID}" ]; then
 	 | python -c "import sys,json;print(json.load(sys.stdin)['success'])")
 	
 	echo $RESULT 
+fi
+
+if [ "$COUNT_RECORD" -eq "0" ];then
+	rm -f /tmp/CERTBOT_count/
 fi
